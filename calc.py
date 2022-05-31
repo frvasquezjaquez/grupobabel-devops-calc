1b3ea4c6a161e8840c963cc42e68438b4ca646ab
Francisco J. Vasquez <34665458+frvasquezjaquez@users.noreply.github.com>
2022-05-30 21:02:50 -0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -3,7 +3,7 @@ class Calc:
         try:
             if num1 < 0 or num2 < 0:
                 return "Invalid"
-            return num1 + num2
+            return num1 + num2 
         except Exception:
             return "Invalid"
 
